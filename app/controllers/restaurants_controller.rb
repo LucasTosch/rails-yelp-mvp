052b165212709f6b800bb803529b3feb759c3f5e
Lucas Toschini <lucas.toschini@live.fr>
2022-04-21 19:59:07 +0200

    exercice terminé avant l improvement

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,28 @@
 class RestaurantsController < ApplicationController
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
+    @restaurant = Restaurant.new
   end
+
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
+
   def show
-
+    @restaurant = Restaurant.find(params[:id])
   end
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
